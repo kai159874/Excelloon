ada98ab23bf909ca5fb8dfad1123b923c307f824
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-05-06 11:06:05 +0000

    [add]balloon controller top

--- app/controllers/public/balloons_controller.rb
+++ app/controllers/public/balloons_controller.rb
@@ -1,14 +1,15 @@
 class Public::BalloonsController < ApplicationController
   before_action :ensure_balloon, only: [:show, :edit, :update, :destroy]
-  
+
   def new
     @balloon = Balloon.new
   end
-  
+
   def create
-    @balloon = current_user.balloon.new(balloon_params)
+    @balloon = current_user.balloons.new(balloon_params)
     if @balloon.save
-      redirect_to balloons_path, flash[:success] = "バルーンを作りました！"
+      flash[:success] = "バルーンを作りました！"
+      redirect_to root_path
     else
       render :new
     end
@@ -19,23 +20,23 @@ class Public::BalloonsController < ApplicationController
 
   def edit
   end
-  
+
   def update
     @balloon.update(balloon_params) ? (redirect_to balloon_path(@balloon)) : (render :edit)
   end
-  
+
   def destroy
     @balloon.destroy ? (redirect_to balloons_path) : (render :edit)
   end
-  
+
   private
-  
-  # def balloon.params
-  #   params.requier(:balloon).permit(:content, :color_status, :user_id)
-  # end
-  
+
+  def balloon_params
+    params.require(:balloon).permit(:content, :color_status, :user_id, :balloon_image)
+  end
+
   def ensure_balloon
     @balloon = Balloon.find(params[:id])
   end
-  
+
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,5 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
+    @balloons = Balloon.all
   end
 
   def about
