43dcfa27dad87bfacb0d1e5aafdde110c1b9dbf3
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-05-15 01:02:44 +0000

    [add]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -36,22 +36,42 @@ User.find_or_create_by!(email: "ray@example.com") do |user|
   user.is_active = true
 end
 
-Balloon.find_or_create_by!(content: "資格勉強〇日目！！") do |balloon|
+Balloon.find_or_create_by!(content: "資格勉強2日目！！") do |balloon|
   balloon.user_id = 1
   balloon.color_status = 1
 end
 
-Balloon.find_or_create_by!(content: "筋トレ継続〇日！まだまだ続けます") do |balloon|
+Balloon.find_or_create_by!(content: "筋トレ継続3日！まだまだ続けます") do |balloon|
   balloon.user_id = 2
-  balloon.color_status = 4
+  balloon.color_status = 2
 end
 
-Balloon.find_or_create_by!(content: "受験まで後〇日！今日は息抜き") do |balloon|
+Balloon.find_or_create_by!(content: "受験まで後100日！今日は息抜き") do |balloon|
   balloon.user_id = 3
-  balloon.color_status = 6
+  balloon.color_status = 3
 end
 
 Balloon.find_or_create_by!(content: "朝早く起きることが出来た！時間作れたので今日はお菓子作りをするよ！！") do |balloon|
+  balloon.user_id = 4
+  balloon.color_status = 4
+end
+
+Balloon.find_or_create_by!(content: "資格勉強30日目！！") do |balloon|
+  balloon.user_id = 1
+  balloon.color_status = 5
+end
+
+Balloon.find_or_create_by!(content: "筋トレ継続522日！まだまだ続けます") do |balloon|
+  balloon.user_id = 2
+  balloon.color_status = 6
+end
+
+Balloon.find_or_create_by!(content: "受験まで後8005日！今日は息抜き") do |balloon|
+  balloon.user_id = 3
+  balloon.color_status = 7
+end
+
+Balloon.find_or_create_by!(content: "朝早く起きることが出来た！時間作れたので今日はお買い物をする！！") do |balloon|
   balloon.user_id = 4
   balloon.color_status = 8
 end
@@ -60,5 +80,7 @@ Sticker.find_or_create_by!(message: "いいね！")
 Sticker.find_or_create_by!(message: "素晴らしい！")
 Sticker.find_or_create_by!(message: "その調子！")
 Sticker.find_or_create_by!(message: "Excellent！")
+Sticker.find_or_create_by!(message: "天才か？")
+Sticker.find_or_create_by!(message: "GO！GO！")
 
 puts "seedの実行を完了しました"
