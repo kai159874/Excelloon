d214805dda2fa88315e69103296696805b82452e
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-05-16 08:57:29 +0000

    [add]follow comment

--- app/controllers/public/relationships_controller.rb
+++ app/controllers/public/relationships_controller.rb
@@ -4,7 +4,7 @@ class Public::RelationshipsController < ApplicationController
   def create
     other_user = User.find(params[:user_id])
     Relationship.create(follower_id: current_user.id, followed_id: other_user.id)
-    if current_user.matchers?(other_user)
+    if current_user.friends?(other_user)
       redirect_to follow_requests_path, notice: "#{other_user.name}さんと友達になりました！"
     else
       redirect_to mypage_path, notice: "友達申請を送りました！"
@@ -13,7 +13,7 @@ class Public::RelationshipsController < ApplicationController
 
   def destroy
     other_user = User.find(params[:user_id])
-    if current_user.matchers?(other_user)
+    if current_user.friends?(other_user)
       current_user.unfollow(other_user)
       other_user.unfollow(current_user)
       redirect_to friends_path, notice: "#{other_user.name}さんを友達リストから解除しました。"
