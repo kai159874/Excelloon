605f49026a36f066cda43f9edec056857fddef4c
KaichiIwahara <157882052+kai159874@users.noreply.github.com>
2024-05-27 07:45:20 +0000

    [add]balloons_search

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,4 +1,5 @@
 class Public::SearchesController < ApplicationController
+  before_action :authenticate_user!
 
   def search
   end
@@ -6,6 +7,12 @@ class Public::SearchesController < ApplicationController
   def user_search
     @keyword = params[:keyword]
     @result = User.find_by('public_uid LIKE ?', @keyword)
+  end
+
+  def balloons_search
+    @keyword = params[:keyword]
+    @stickers = Sticker.all
+    @balloons = Balloon.where("content LIKE ?", "%#{@keyword}%").where_user_active.includes(:user).order(id: "DESC").page(params[:page]).per(20)
   end
 
 end
